ed1ea25e74fd7a5c30f87bc4b4b7babb74e87e1b
Richard Littauer <richard.littauer@gmail.com>
2014-05-02 13:10:32 -0400

    Initial work and yo.

---
+++ dist/scripts/5290fec7.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("ourGroundsApp",[]),angular.module("ourGroundsApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
